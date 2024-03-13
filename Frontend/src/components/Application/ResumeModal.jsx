5023cb3e31910013cb1b602acc548aa5f61ac211
Saurabh <prashantrangdale1234@gmail.com>
2024-03-13 08:16:28 +0530

    MyApplcation see from 7:30:40

--- Frontend/src/components/Application/ResumeModal.jsx
+++ Frontend/src/components/Application/ResumeModal.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const ResumeModal = () => {
+const ResumeModal = ({imageUrl, onClose}) => {
   return (
-    <></>
+    <>
+      <div className="resume-modal">
+        <div className="modal-content">
+          <span className="close" onClick={onClose}>
+            &times;
+          </span>
+          <img src={imageUrl} alt="resume" />
+        </div>
+      </div>
+    </>
   )
 }
 
