911cbd4d10f2b03b44e9ef445493f6b75495e8a6
Saurabh <prashantrangdale1234@gmail.com>
2024-04-06 07:06:47 +0530

    frontend changed

--- Frontend/src/components/Job/JobDetails.jsx
+++ Frontend/src/components/Job/JobDetails.jsx
@@ -15,7 +15,7 @@ const JobDetails = () => {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:6005/api/v1/job/${id}`, {
+      .get(`https://job-seeking-website-seven.vercel.app/api/v1/job/${id}`, {
         withCredentials: true,
       })
       .then((res) => {
